2802d1933016c70fa4879cbc20c7f1dccfbf159b
Ben Mitchell <bjosephmitchell@gmail.com>
2022-12-01 16:19:33 +0000

    Skeleton layout for left panel

--- src/qobject/LeftPanelWidget.cpp
+++ src/qobject/LeftPanelWidget.cpp
@@ -5,6 +5,21 @@
 namespace animation_cw3::qobject {
 LeftPanelWidget::LeftPanelWidget(MainWindow* parent)
     : QWidget(parent)
+    , m_Params({
+          .tankDimensions = { 5, 5 },
+          .waterDimensions = { 2, 3 },
+          .initialWaterPosition = { 0, 4 },
+          .isRunning = false,
+      })
+    , m_Layout(new QVBoxLayout(this))
+    , m_StartButton(new QPushButton("Start", this))
+    , m_PauseButton(new QPushButton("Pause", this))
+    , m_ResetButton(new QPushButton("Reset", this))
 {
+    setLayout(m_Layout);
+    m_Layout->setAlignment(Qt::AlignTop);
+    m_Layout->addWidget(m_StartButton);
+    m_Layout->addWidget(m_PauseButton);
+    m_Layout->addWidget(m_ResetButton);
 }
 }
--- src/qobject/LeftPanelWidget.h
+++ src/qobject/LeftPanelWidget.h
@@ -1,13 +1,34 @@
 #pragma once
 
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QVector2D>
 #include <QWidget>
 
 namespace animation_cw3::qobject {
 class LeftPanelWidget : public QWidget {
     Q_OBJECT
+private:
+    struct AnimationParameters {
+        QVector2D tankDimensions; // Dimensions of the tank in meters
+        QVector2D waterDimensions; // Initial dimensions of the water in the tank in meters
+        QVector2D initialWaterPosition; // Initial position of the water in the tank in meters
+        bool isRunning;
+    } m_Params;
+
+    QVBoxLayout* m_Layout;
+    QPushButton* m_StartButton;
+    QPushButton* m_PauseButton;
+    QPushButton* m_ResetButton;
+
 public:
     LeftPanelWidget(class MainWindow* parent = nullptr);
 
+    inline const AnimationParameters& animationParameters() { return m_Params; }
+
 signals:
+    void startAnimation();
+    void pauseAnimation();
+    void resetAnimation();
 };
 }
